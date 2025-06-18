fac264ca0be0cca1012c10a7106b51d76258e7a5
Agustin <agusvalussi10@gmail.com>
2025-06-18 16:37:32 -0300

    Terminando Ejercicios

--- TP ITEGRADOR - UFERER/KIOSCO/SRC/.H/administracion.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADMINISTRACION_H
-#define ADMINISTRACION_H
-
-#include "producto.h"  // Necesario para usar Producto
-
-void menuAdministrativo(Producto productos[], int *cantidadProductos);
-void mostrarStockBajo(Producto productos[], int cantidadProductos);
-
-#endif
